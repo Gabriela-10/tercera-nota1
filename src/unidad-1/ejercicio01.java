eb72a64c25b60b2b2d23c4e1a28407cb873743f2
Gaby <gvergelj@ufpso.edu.co>
2026-09-04 16:43:46 -0500

    ejercicio01.java

---
+++ src/unidad-1/ejercicio01.java
@@ -0,0 +1,12 @@
+public class ejercicio01 {
+    public static void main(String[] args) throws Exception {
+        int edad = 17;
+        double estatura = 1.55;
+        char inicial = 'G';
+        boolean mayorEdad = false;
+        System.out.println(" edad: " + edad);
+        System.out.println(" estatura: " + estatura);
+        System.out.println(" inicial: " + inicial);
+        System.out.println(" mayor de edad: " + mayorEdad);
+    }
+}
